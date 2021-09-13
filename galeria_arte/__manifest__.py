b6a40162d1568507507e9f2be7b9b2a4ba4bc345
Marta Taulet <mta@odoo.com>
2021-09-13 18:32:05 +0000

    [ADD] galeria_arte

---
+++ galeria_arte/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Galeria de Arte',
+    
+    'summary': """Galeria de Arte""",
+    
+    'description': """
+        Modulo de Galeria de Arte para gestionar:
+        - Obras de Arte
+        - Artistas
+        - Donaciones
+    """,
+    
+    'author': 'Odoo', 
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training', 
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+    ],
+    
+    'demo': [
+    ],
+}
